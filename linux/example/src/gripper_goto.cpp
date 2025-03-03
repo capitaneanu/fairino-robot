ee2324b47acf0a378b1708060f660313ac2dbd94
Daniel Capitaneanu <capitaneanu@gmail.com>
2025-03-03 14:22:02 +0200

    some changes

--- linux/example/src/gripper_goto.cpp
+++ linux/example/src/gripper_goto.cpp
@@ -49,56 +49,9 @@ int main(int argc, char **argv)
     int8_t speed = 0;
     int8_t gstatus = 1;
 
-    // robot.SetGripperConfig(company, device, softversion, bus);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    // robot.GetGripperConfig(&company, &device, &softversion, &bus);
-    // printf("gripper config:%d,%d,%d,%d\n", company, device, softversion, bus);
-
-    // robot.ActGripper(index, act);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    // robot.GetGripperActivateStatus(&fault, &active_status);
-    // printf("gripper active fault is: %u, status is: %u\n", fault, active_status);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-
-    // act = 1;
-    // robot.ActGripper(index, act);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    // robot.GetGripperActivateStatus(&fault, &active_status);
-    // printf("gripper active fault is: %u, status is: %u\n", fault, active_status);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
     robot.MoveGripper(index, gpos, 50, 40, max_time, block, 0, 0, 0, 0);
-
-    // while (gstatus)
-    // {
-    //     status = 1;
-    //     robot.GetGripperMotionDone(&fault, &status);
-    //     if (status == 3)
-    //     {
-    //         gstatus = 0;
-    //     }
-    // }
-
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-    // robot.GetGripperCurPosition(&fault, &current_pos);
-    // printf("fault is:%u, current position is: %u\n", fault, current_pos);
-
-    // std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-    // robot.GetGripperCurCurrent(&fault, &current);
-    // printf("fault is:%u, current current is: %d\n", fault, current);
-
-    // robot.GetGripperVoltage(&fault, &voltage);
-    // printf("fault is:%u, current voltage is: %d \n", fault, voltage);
-
-    // robot.GetGripperTemp(&fault, &temp);
-    // printf("fault is:%u, current temperature is: %d\n", fault, temp);
-
-    // robot.GetGripperCurSpeed(&fault, &speed);
-    // printf("fault is:%u, current speed is: %d\n", fault, speed);
 
     printf("\n");
 
