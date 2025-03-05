7921272fd28698b304253625b496a8506910adb9
Daniel Capitaneanu <capitaneanu@gmail.com>
2025-03-05 19:24:59 +0200

    code cleaning

--- linux/example/src/gripper_pp.cpp
+++ linux/example/src/gripper_pp.cpp
@@ -124,21 +124,6 @@ int main(void) {
     err0 = robot.MoveJ(&pj4, &desc_pos4, tool, user, vel, acc, ovl, &epos,
                        blendT, flag, &offset_pos);
     robot.MoveGripper(index, 0, 60, 40, max_time, block, 0, 0, 0, 0);
-
-    // err0 = robot.MoveJ(&pj1, &desc_pos1, tool, user, vel, acc, ovl, &epos,
-    // blendT, flag, &offset_pos); robot.MoveGripper(index, 0, 60, 40, max_time,
-    // block, 0, 0, 0, 0);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    // err0 = robot.MoveJ(&pj3, &desc_pos3, tool, user, vel, acc, ovl, &epos,
-    // blendT, flag, &offset_pos);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    // err0 = robot.MoveJ(&pj4, &desc_pos4, tool, user, vel, acc, ovl, &epos,
-    // blendT, flag, &offset_pos);
-    // err0 = robot.MoveJ(&pj1, &desc_pos1, tool, user, vel, acc, ovl, &epos,
-    // blendT, flag, &offset_pos); robot.MoveGripper(index, 100, 60, 40,
-    // max_time, block, 0, 0, 0, 0);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
     robot.MoveGripper(index, 60, 60, 40, max_time, block, 0, 0, 0, 0);
     err0 = robot.MoveJ(&pj3, &desc_pos3, tool, user, vel, acc, ovl, &epos,
                        blendT, flag, &offset_pos);
